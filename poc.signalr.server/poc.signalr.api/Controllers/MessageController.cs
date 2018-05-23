56da51249776c8b1383710c8759b3239b0ae2b22
brunno <web.brunno@gmail.com>
2018-05-23 12:22:21 -0300

    Cria projeto.

---
+++ poc.signalr.server/poc.signalr.api/Controllers/MessageController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace poc.signalr.api.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/[controller]")]
+    public class MessageController : Controller
+    {
+        public IActionResult Post() {
+            return Ok();
+        }
+    }
+}
---
+++ poc.signalr.server/poc.signalr.api/Hubs/MessageHub.cs
@@ -0,0 +1,12 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR;
+
+namespace poc.signalr.api.Hubs {
+
+    public class MessageHub : Hub {
+
+        public Task Send(string message) {
+            return Clients.All.InvokeAsync("Send", message);
+        }
+    }
+}
